058e444b319dd8d7936a7afb2d19754b56e83f97
lxh <lixianghe2002@126.com>
2023-11-01 15:54:39 +0800

    添加接口

--- table/test1.py
+++ table/test1.py
@@ -3,14 +3,32 @@ import csv
 
 app = Flask(__name__)
 
-@app.route('/create_user', methods=['POST'])
+@app.route('/create_user', methods=['POST'],endpoint="userinfo")
 def create_user():
     user_data = request.json
-    with open('user_profile.csv', mode='a', newline='') as file:
+    with open('user_info.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(user_data.values())
 
     return jsonify({'status': 'success'})
+
+@app.route('/user_ip', methods=['POST'],endpoint="userip")
+def create_user():
+    user_data = request.json
+    with open('user_ip.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(user_data.values())
+
+    return jsonify({'status': 'successed'})
+
+@app.route('/user_time', methods=['POST'],endpoint="usertime")
+def create_user():
+    user_data = request.json
+    with open('user_time.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(user_data.values())
+
+    return jsonify({'status': 'successed'})
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=5001,debug=True)
--- table/test3.py
+++ table/test3.py
@@ -1,29 +1,31 @@
 import pandas as pd
 import numpy as np
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
 import warnings
 import heapq
 from scipy.sparse import csr_matrix
 
-# 稀疏矩阵来存储相似度矩阵，使用了DBSCAN算法来进行聚类，避免了重复计算，使用了更高效的数据结构来处理用户画像数据。
-
-
-# 禁用scikit-learn库的FutureWarning警告
-warnings.filterwarnings("ignore", category=FutureWarning, module="sklearn")
-
-# 读取用户画像数据
+# 读取用户画像数据并进行数据清洗和验证
 user_profile = pd.read_csv('user_profile.csv')
+user_profile.dropna(inplace=True) # 删除缺失值
+user_profile['age'] = pd.to_numeric(user_profile['age'], errors='coerce')
+user_profile.dropna(subset=['age'], inplace=True) # 删除无效值
+user_profile.reset_index(drop=True, inplace=True)
+
 # 设定每位用户推荐的人数
-num_recommendations = 2
+num_recommendations = 3
+
 # 添加星座标签权重
 tag_weights = {'age': 0.2, 'gender': 0.3, 'interests': 0.5, 'constellation': 0.1}
 
+# 对不同标签进行归一化处理
 # 对不同标签进行归一化处理
 for tag in tag_weights.keys():
     if tag == 'constellation':
         user_profile[tag] = user_profile[tag].apply(lambda x: ord(x[0]) - 64)
     else:
         user_profile[tag] = pd.to_numeric(user_profile[tag], errors='coerce')
+        user_profile[tag] = user_profile[tag].fillna(0) # 将缺失值填充为0
         user_profile[tag] = (user_profile[tag] - user_profile[tag].min()) / (user_profile[tag].max() - user_profile[tag].min())
 
 # 计算用户之间的相似度矩阵
@@ -42,19 +44,18 @@ similarity_matrix = tag_weights['age'] * age_diff + tag_weights['gender'] * gend
 np.fill_diagonal(similarity_matrix, 1)
 similarity_matrix_sparse = csr_matrix(similarity_matrix)
 
-# 对相似度矩阵进行模糊聚类
-dbscan = DBSCAN(eps=0.5, min_samples=5, metric='precomputed')
-dbscan.fit(similarity_matrix_sparse)
+# 对相似度矩阵进行聚类
+kmeans = KMeans(n_clusters=10, random_state=0).fit(similarity_matrix_sparse)
 
 # 对每个用户进行推荐
 for i in range(len(user_profile)):
     recommendations = [] # 初始化recommendations列表
     for j in range(len(user_profile)):
-        if i != j and dbscan.labels_[i] == dbscan.labels_[j]:
+        if i != j and kmeans.labels_[i] == kmeans.labels_[j]:
             weighted_similarity = similarity_matrix[i][j] * user_profile.iloc[j]['weight'] * (1 - abs(user_profile.iloc[i]['constellation'] - user_profile.iloc[j]['constellation']) / 12)
             recommendations.append((user_profile.iloc[j]['name'], weighted_similarity)) # 将相似的用户添加到recommendations列表中
     recommendations = [rec for rec in recommendations if rec[0] not in user_profile.iloc[i]['name']]
     recommendations = heapq.nlargest(num_recommendations, recommendations, key=lambda x: x[1])
-    print(f"-----------------为用户{user_profile.iloc[i]['name']}推荐可能认识的人：")
+    print(f"----------{user_profile.iloc[i]['name']}推荐可能认识的人---------------")
     for rec in recommendations:
-        print(f"用户{rec[0]}，相似度得分：{rec[1]}")
+        print(f"{rec[0]}，相似度得分：{rec[1]}")
